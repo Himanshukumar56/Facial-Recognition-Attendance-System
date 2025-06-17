47c147a039c4a024d36274f804fd13a9568de3f7
Himanshu Kumar <himanshu.2201056cs@iiitbh.ac.in>
2025-06-18 00:38:23 +0530

    Started working on the project

---
+++ add_faces.py
@@ -0,0 +1,25 @@
+import cv2
+
+video = cv2.VideoCapture(0)
+facedetect = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+
+faces_data = []
+#empty list to  collect the data of all the people
+
+while True:
+    ret, frame = video.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = facedetect.detectMultiScale(gray, 1.3, 5)
+
+    for (x,y,w,h) in faces:
+        crop_img = frame[y:y+h, x:x+w, :]
+        resized_img = cv2.resize(crop_img, (50,50))
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (50,50,250), 1)
+
+    cv2.imshow("frame", frame)
+    k = cv2.waitKey(1)
+    if k == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
